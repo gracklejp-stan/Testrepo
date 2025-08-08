94a70e05c9cb147d16417f169c80bf939126f700
gracklejp-stan <gracklejp@gmail.com>
2025-08-08 09:45:08 -0700

    Update Root.tsx

--- Custom/Root.tsx
+++ Custom/Root.tsx
@@ -27,7 +27,6 @@ import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import { MyGroupsSidebarItem } from '@backstage/plugin-org';
 import GroupIcon from '@material-ui/icons/People';
-import { HomescreenappPage } from '@internal/plugin-homescreenapp';
 
 const useSidebarLogoStyles = makeStyles({
   root: {
